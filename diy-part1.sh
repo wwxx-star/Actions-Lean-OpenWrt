8376dbfeefba60538125535f2129c7205caf3aab
lxhao61 <751142257@qq.com>
2020-07-16 07:45:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,16 +8,8 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# Uncomment a feed source
 # 取消helloworld源注释（ssr-plus）
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# 取消telephony源注释
-#sed -i 's/^#\(.*telephony\)/\1/' feeds.conf.default
 
-# comment a feed source
-# 注释掉原lean大luci源
-#sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
-
-# Add a feed source
 # 添加lienol大luci源
 #sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;dev-18.06' feeds.conf.default
