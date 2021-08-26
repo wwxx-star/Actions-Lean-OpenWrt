3a7dc467fd251d6a447b2dbe5f59492394c32aa9
lxhao61 <lxhao61@gmail.com>
2021-08-26 10:43:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,26 +22,6 @@ rm -rf package/lean/trojan
 # 拉取luci-app-smartdns插件
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lienol/luci-app-smartdns
 
-# 拉取passwall插件及依赖
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/lienol/brook
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/lienol/chinadns-ng
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/openssl1.1 package/lienol/openssl1.1
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/lienol/tcping
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/lienol/dns2socks
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/lienol/ipt2socks
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/lienol/pdnsd-alt
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/kcptun package/lienol/kcptun
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocksr-libev package/lienol/shadowsocksr-libev
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray package/lienol/v2ray
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/simple-obfs package/lienol/simple-obfs
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin package/lienol/v2ray-plugin
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan package/lienol/trojan
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/lienol/ssocks
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/lienol/trojan-go
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/lienol/trojan-plus
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/lienol/naiveproxy
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/lienol/luci-app-passwall
-
 # 拉取passwall源码
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 #git clone https://github.com/lxhao61/openwrt-passwall.git package/passwall
