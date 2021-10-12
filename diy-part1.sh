70aa2ff815034632b879d88b05b0d670790a9044
lxhao61 <lxhao61@gmail.com>
2021-10-12 08:39:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,4 +29,4 @@
 sed -i 's/^\(.*telephony\)/#&/' feeds.conf.default
 
 # 添加openwrt-18.06 telephony源
-sed -i '$a src-git https://github.com/lxhao61/telephony.git' feeds.conf.default
+sed -i '$a src-git telephony https://github.com/lxhao61/telephony.git' feeds.conf.default
