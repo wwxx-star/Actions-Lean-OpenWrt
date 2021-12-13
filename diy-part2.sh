b094ce3f6a984f78f5861caaaf3e5be1b2bf841e
lxhao61 <lxhao61@gmail.com>
2021-12-13 15:00:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,6 +26,3 @@ svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns packa
 # 拉取passwall源码
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 #git clone https://github.com/lxhao61/openwrt-passwall.git package/passwall
-
-# 删除passwall中xray-core核心
-#rm -rf package/passwall/xray-core
