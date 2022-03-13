8430de4c29be76b39e2b33d7e6e467bfcbfe864b
lxhao61 <lxhao61@gmail.com>
2022-03-13 16:02:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,14 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
 # 拉取passwall源码
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/packages
 #git clone https://github.com/lxhao61/openwrt-passwall.git package/passwall
 
 # 拉取luci-app-passwall插件
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/passwall/luci-app-passwall
-svn co https://github.com/lxhao61/openwrt-packages/trunk/luci-app-passwall package/passwall/luci-app-passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+#svn co https://github.com/lxhao61/openwrt-packages/trunk/luci-app-passwall package/passwall/luci-app-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci
+#git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci
 
 # 拉取luci-app-smartdns插件
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lienol/luci-app-smartdns
