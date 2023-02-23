15b0129f97d3f45c0e2d21fe019990ec68e7cf2c
lxhao61 <lxhao61@gmail.com>
2023-02-24 04:17:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,14 +17,11 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 取消Lean大登陆密码
 sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
-# 拉取passwall源码
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/packages
-#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/packages
-
-# 拉取luci-app-passwall插件
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci
-#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/luci
-#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/passwall/luci-app-passwall
+# 拉取PassWall源码
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/packages
+#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/packages
+git clone -b luci-reality https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
+#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
 
 # 拉取luci-app-smartdns插件
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lienol/luci-app-smartdns
