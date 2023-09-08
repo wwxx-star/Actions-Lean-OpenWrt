71cb19461481a687e2643c20bb7e8d127e8b9311
lxhao61 <lxhao61@gmail.com>
2023-09-09 04:18:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,12 +19,21 @@ sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-sett
 
 # 拉取PassWall源码
 git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/packages
-#cd package/xiaorouji/packages
-#git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
-#cd -
+cd package/xiaorouji/packages
+git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
+cd -
 #git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/packages
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
-#git clone -b luci-reality https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
+cd package/xiaorouji/luci
+git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
+cd -
+#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
+
+# 删除自带phtunnel源码
+#rm -rf package/feeds/packages/xray-core
+
+# 拉取phtunnel、pgyvpn源码
+#git clone https://github.com/OrayOS/OpenOray.git package/OpenOray
 
 # 拉取luci-app-smartdns插件
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lienol/luci-app-smartdns
