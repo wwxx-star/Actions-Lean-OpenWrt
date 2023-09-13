edc7896b653a9f3fa540e9e4dbcc8c598206b14a
lxhao61 <lxhao61@gmail.com>
2023-09-14 03:08:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,13 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
 
 # 拉取PassWall源码
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/xiaorouji/packages
-#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/packages
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/feeds/packages
+#git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/feeds/packages
 cd package/xiaorouji/packages
 git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
 cd -
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
-#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci
+#git clone -b luci https://github.com/lxhao61/openwrt-passwall.git package/feeds/luci
 cd package/xiaorouji/luci
 git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 cd -
